64f2497ce16d9f866923860e7f327036ef7d000e
EmmyConceptNg <emmyconceptng@gmail.com>
2023-06-09 05:01:28 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,75 @@
-import React from "react";
-import { Button } from "@chakra-ui/react";
-import "./Navbar.css";
+import { Button } from '@chakra-ui/react';
+import './Navbar.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div className="footer_div">
-      <div className="footer_inner_container">
-        <div className="footer_section_one">
-          <div className="fast">Fast</div>
-          <div className="contact">Contact</div>
-          <div className="services">Do you want to use our services?</div>
-          <div className="questions"> Do you have additional questions?</div>
-        </div>
-        <div className="footer_section_two">
-          <div className="numbers">
-            <div className="_one"> + 48 662 969 662</div>
-            <div className="_two">( New Market )</div>
-            <div className="_three">+ 48 887 770 774 </div>
-            <div className="_four"></div> ( Białka Tatrzańska )
-          </div>
-        </div>
-        <div className="footer_section_three">
-          <div className="reach_button">
+    <>
+      <Container
+        className="mt-5 px-5 py-5 "
+        style={{
+          zIndex: '9',
+          backgroundColor: '#1a1a1a',
+          minWidth: '100vw',
+          boxShadow: ' 0 4px 30px rgba(0, 0, 0, 0.1)',
+          backdropFilter: 'blur(5px)',
+          '-webkit-backdrop-filter': 'blur(5px)',
+        }}
+      >
+        <Row xs={2} md={3} lg={3} className="mx-auto  ">
+          <Col className="my-auto">
+            {' '}
+            <h3 className="text-white fast">Fast</h3>
+            <h2 className="fw-bold text-white">CONTACT</h2>
+            <h6 className="text-white">Do you want to use our services?</h6>
+            <h6 className="text-white">Do you have additional questions?</h6>
+          </Col>
+          <Col className="my-auto">
+            <h6 className="text-white">+ 48 662 969 662 ( New Market )</h6>
+            <h6 className="text-white">
+              + 48 887 770 774 ( Białka Tatrzańska )
+            </h6>
+          </Col>
+          <Col className="my-auto">
             <Button
-              backgroundColor={"rgba(0, 189, 93, 1)"}
-              className="btn_green"
+              backgroundColor={'rgba(0, 189, 93, 1)'}
+              className="btn_green text-white"
             >
               Reach out to us
             </Button>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <div className="footer_div">
+        <div className="footer_inner_container">
+          <div className="footer_section_one">
+            <div className="fast">Fast</div>
+            <div className="contact">Contact</div>
+            <div className="services">Do you want to use our services?</div>
+            <div className="questions"> Do you have additional questions?</div>
+          </div>
+          <div className="footer_section_two">
+            <div className="numbers">
+              <div className="_one"> + 48 662 969 662</div>
+              <div className="_two">( New Market )</div>
+              <div className="_three">+ 48 887 770 774 </div>
+              <div className="_four"></div> ( Białka Tatrzańska )
+            </div>
+          </div>
+          <div className="footer_section_three">
+            <div className="reach_button">
+              <Button
+                backgroundColor={'rgba(0, 189, 93, 1)'}
+                className="btn_green"
+              >
+                Reach out to us
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 };
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,54 +1,93 @@
-import React from "react";
-import "./Pages.css";
-import BlurryBlob from "../components/BlurryBlob";
+import './Pages.css';
+import BlurryBlob from '../components/BlurryBlob';
+import {  Container, Form } from 'react-bootstrap';
+import { Button } from '@chakra-ui/react';
 
 // import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 const Contact = () => {
   return (
-    <div>
-      <div className="insurance_body">
-        <div className="first_banner">
-          <BlurryBlob
-            height={90}
-            style={{
-              position: "absolute",
-              top: "-35rem",
-              left: "auto",
-              right: "auto",
-              marginLeft: "0",
-              marginRight: "0",
-              zIndex: "0 !important"
-            }}
-          />
-        </div>
-        <div className="arc_container">
-          <div className="top_arc" style={{ top: "10rem" }}></div>
-        </div>
-        <div className="second_banner">
-          <div className="background_ball">
-            <div
-              className="_low_price"
-              style={{ marginTop: "9rem", fontSize: "1.8rem", color: "white" }}
-            >
-              _{" "}
-            </div>
-            <div
-              className="_insurance"
+    <>
+      <div>
+        <div className="insurance_body">
+          <div className="first_banner">
+            <BlurryBlob
+              height={90}
               style={{
-                marginTop: "-11rem",
-                fontSize: "2.5rem",
-                color: "white",
-                fontWeight: "bold"
+                position: 'absolute',
+                top: '-35rem',
+                left: 'auto',
+                right: 'auto',
+                marginLeft: '0',
+                marginRight: '0',
+                zIndex: '0 !important',
               }}
-            >
-              CONTACT US{" "}
+            />
+          </div>
+          <div className="arc_container">
+            <div className="top_arc" style={{ top: '10rem' }}></div>
+          </div>
+          <div className="second_banner">
+            <div className="background_ball">
+              <div
+                className="_low_price"
+                style={{
+                  marginTop: '9rem',
+                  fontSize: '1.8rem',
+                  color: 'white',
+                }}
+              >
+                _{' '}
+              </div>
+              <div
+                className="_insurance"
+                style={{
+                  marginTop: '-11rem',
+                  fontSize: '2.5rem',
+                  color: 'white',
+                  fontWeight: 'bold',
+                }}
+              >
+                CONTACT US{' '}
+              </div>
             </div>
           </div>
+          <Container style={{ zIndex: '999' }}>
+            <Form
+              className="p-5 mx-auto mt-2"
+              style={{
+                width: '700px',
+                backgroundColor: 'rgba(0, 189, 93, 1)',
+              }}
+            >
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>name</Form.Label>
+                <Form.Control type="email" placeholder="Enter name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We will never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+
+              <Button
+                backgroundColor={'rgba(0, 189, 93, 1)'}
+                className="btn_green text-white"
+              >
+                Submit
+              </Button>
+            </Form>
+          </Container>
+          {/* <Wrapper apiKey={"YOUR_API_KEY"} render={render}></Wrapper> */}
         </div>
-        {/* <Wrapper apiKey={"YOUR_API_KEY"} render={render}></Wrapper> */}
       </div>
-    </div>
+    </>
   );
 };
 
