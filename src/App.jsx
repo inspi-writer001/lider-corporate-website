685fe689e67ac82c514b636412e24022891f4fd1
EmmyConceptNg <emmyconceptng@gmail.com>
2023-06-09 11:29:22 +0100

    Live chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Contact from "./pages/Contact";
-import Document from "./pages/Document";
-import Download from "./pages/Download";
-import Home from "./pages/Home";
-import Insurance from "./pages/Insurance";
-import Registration from "./pages/Registration";
-import Translations from "./pages/Translation";
-import { theme } from "./theme/theme";
-import Footer from "./components/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
-// import Client from './liveChat/Client';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './pages/Contact';
+import Document from './pages/Document';
+import Download from './pages/Download';
+import Home from './pages/Home';
+import Insurance from './pages/Insurance';
+import Registration from './pages/Registration';
+import Translations from './pages/Translation';
+import { theme } from './theme/theme';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Client from './liveChat/Client';
 
 function App() {
   return (
     <div
       className="App"
       style={{
-        backgroundImage: theme.dark_background
+        backgroundImage: theme.dark_background,
       }}
     >
       <div className="noise"></div>
       <Router>
         <Navbar />
-        <div style={{ height: "90px" }} />
+        <div style={{ height: '90px' }} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/insurance" element={<Insurance />} />
@@ -34,7 +34,7 @@ function App() {
           <Route path="/documents" element={<Document />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        {/* <Client /> */}
+        <Client />
 
         <Footer />
       </Router>
