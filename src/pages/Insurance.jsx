84923afa21ef1198b862090020cfd61138d26a76
EmmyConceptNg <emmyconceptng@gmail.com>
2023-06-13 13:45:08 +0100

    Translation

--- src/pages/Insurance.jsx
+++ src/pages/Insurance.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import "./Pages.css";
-import BlurryBlob from "../components/BlurryBlob";
-import PartnersList from "../components/PartnersList";
+import React from 'react';
+import './Pages.css';
+import BlurryBlob from '../components/BlurryBlob';
+import PartnersList from '../components/PartnersList';
+import { t } from 'i18next';
 
 const Insurance = () => {
   return (
@@ -11,74 +12,62 @@ const Insurance = () => {
           <BlurryBlob
             height={90}
             style={{
-              position: "absolute",
-              top: "-35rem",
-              left: "auto",
-              right: "auto",
-              marginLeft: "0",
-              marginRight: "0",
-              zIndex: "0 !important"
+              position: 'absolute',
+              top: '-35rem',
+              left: 'auto',
+              right: 'auto',
+              marginLeft: '0',
+              marginRight: '0',
+              zIndex: '0 !important',
             }}
           />
         </div>
         <div className="arc_container">
-          <div className="top_arc" style={{ top: "10rem" }}></div>
+          <div className="top_arc" style={{ top: '10rem' }}></div>
         </div>
         <div className="second_banner">
           <div className="background_ball">
-            <div className="_low_price">LOW PRICE</div>
-            <div className="_insurance">INSURANCE</div>
+            <div className="_low_price">{t('insurance.price')}</div>
+            <div className="_insurance">{t('insurance.insurance')}</div>
           </div>
         </div>
         <div className="another_div">
           <div className="top_div">
-            <div className="left_" style={{ fontSize: "2.5rem" }}>
-              Why Us?
+            <div className="left_" style={{ fontSize: '2.5rem' }}>
+              {t('insurance.why_us')}
             </div>
-            <div className="right_">
-              All risks, i.e. insurance against all risks - AC insurance covers
-              damage to the vehicle, its destruction or loss due to all events.
-            </div>
+            <div className="right_">{t('insurance.text1')}</div>
           </div>
           <div className="bottom_div">
-            <div className="left_ a">
-              The subject of insurance is the civil liability of any person who,
-              while driving a motor vehicle during the duration of the insurance
-              contract, caused damage in connection with the movement of that
-              vehicle.
-            </div>
-            <div className="right_ a">
-              “ Lider "offers you the best offers on the market of known
-              insurance companies. We guarantee the greatest professionalism and
-              advice regarding vehicle insurance.
-            </div>
+            <div className="left_ a">{t('insurance.text2')}</div>
+            <div className="right_ a">{t('insurance.text3')}</div>
           </div>
         </div>
         <div className="arc_container">
           <div
             className="top_arc"
-            style={{ top: "-18rem", transform: "rotate(180deg)" }}
+            style={{ top: '-18rem', transform: 'rotate(180deg)' }}
           ></div>
         </div>
-        <div className="best_offer second_banner" style={{ top: "12rem" }}>
+        <div className="best_offer second_banner" style={{ top: '12rem' }}>
           <div className="background_ball">
             <div
               className="_low_price"
-              style={{ marginTop: "9rem", fontSize: "1.8rem", color: "white" }}
+              style={{ marginTop: '9rem', fontSize: '1.8rem', color: 'white' }}
             >
-              Best
+              {t('insurance.best')}
             </div>
             <div
               className="_insurance"
               style={{
-                marginTop: "-11rem",
-                fontSize: "2.5rem",
-                color: "white",
-                fontWeight: "bold",
-                textTransform: "uppercase"
+                marginTop: '-11rem',
+                fontSize: '2.5rem',
+                color: 'white',
+                fontWeight: 'bold',
+                textTransform: 'uppercase',
               }}
             >
-              Offers
+              {t('insurance.offers')}
             </div>
           </div>
         </div>
@@ -86,19 +75,19 @@ const Insurance = () => {
           <BlurryBlob
             height={90}
             style={{
-              position: "absolute",
-              top: "50rem",
-              left: "auto",
-              right: "auto",
-              marginLeft: "0",
-              marginRight: "0",
-              zIndex: "0 !important"
+              position: 'absolute',
+              top: '50rem',
+              left: 'auto',
+              right: 'auto',
+              marginLeft: '0',
+              marginRight: '0',
+              zIndex: '0 !important',
             }}
           />
         </div>
         <div className="partners">
           <div className="those_container">
-            <div className="those">THOSE WHO TRUST US</div>
+            <div className="those">{t('insurance.trust')}</div>
             {/* <div className="those">THOSE</div> */}
           </div>
           <div className="partners_">
@@ -109,7 +98,7 @@ const Insurance = () => {
                     <img
                       src={item.img}
                       alt={item.desc}
-                      style={{ width: "9rem" }}
+                      style={{ width: '9rem' }}
                     />
                   </div>
                 );
