94ddd968c7344f4a4ea43215c757f2f80dbc0753
inspi-writer <conservedinnovation@gmail.com>
2023-06-14 07:04:38 +0100

    neeew

--- src/pages/Document.jsx
+++ src/pages/Document.jsx
@@ -1,6 +1,6 @@
-import './Pages.css';
-import BlurryBlob from '../components/BlurryBlob';
-import { useTranslation } from 'react-i18next';
+import "./Pages.css";
+import BlurryBlob from "../components/BlurryBlob";
+import { useTranslation } from "react-i18next";
 
 const Document = () => {
   const { t } = useTranslation();
@@ -11,82 +11,82 @@ const Document = () => {
           <BlurryBlob
             height={90}
             style={{
-              position: 'absolute',
-              top: '-35rem',
-              left: 'auto',
-              right: 'auto',
-              marginLeft: '0',
-              marginRight: '0',
-              zIndex: '0 !important',
+              position: "absolute",
+              top: "-35rem",
+              left: "auto",
+              right: "auto",
+              marginLeft: "0",
+              marginRight: "0",
+              zIndex: "0 !important"
             }}
           />
         </div>
         <div className="arc_container">
-          <div className="top_arc" style={{ top: '10rem' }}></div>
+          <div className="top_arc" style={{ top: "10rem" }}></div>
         </div>
         <div className="second_banner">
           <div className="background_ball">
             <div
               className="_low_price"
-              style={{ marginTop: '9rem', fontSize: '1.8rem', color: 'white' }}
+              style={{ marginTop: "9rem", fontSize: "1.8rem", color: "white" }}
             >
-              {t('documents.design')}{' '}
+              {t("documents.design")}{" "}
             </div>
             <div
               className="_insurance"
               style={{
-                marginTop: '-11rem',
-                fontSize: '2.5rem',
-                color: 'white',
-                fontWeight: 'bold',
+                marginTop: "-11rem",
+                fontSize: "2.5rem",
+                color: "white",
+                fontWeight: "bold"
               }}
             >
-              {t('documents.documents')}{' '}
+              {t("documents.documents")}{" "}
             </div>
           </div>
         </div>
         <div className="another_div">
           <div className="top_div">
-            <div className="left_" style={{ fontSize: '2.5rem' }}>
-              {t('documents.why_us')}
+            <div className="left_" style={{ fontSize: "2.5rem" }}>
+              {t("documents.why_us")}
             </div>
-            <div className="right_">{t('documents.text1')}</div>
+            <div className="right_">{t("documents.text1")}</div>
           </div>
           <div className="second_banner">
-            <div className="background_ball" style={{ top: '0' }}>
+            <div className="background_ball" style={{ top: "0" }}>
               <div
                 className="_low_price"
                 style={{
-                  marginTop: '9rem',
-                  fontSize: '1.8rem',
-                  color: 'white',
+                  marginTop: "9rem",
+                  fontSize: "1.8rem",
+                  color: "white"
                 }}
               >
-                {t('documents.instant')}
+                {t("documents.instant")}
               </div>
               <div
                 className="_insurance"
                 style={{
-                  marginTop: '-11rem',
-                  fontSize: '2.5rem',
-                  color: 'white',
-                  fontWeight: 'bold',
+                  marginTop: "-11rem",
+                  fontSize: "2.5rem",
+                  color: "white",
+                  fontWeight: "bold"
                 }}
               >
-                {t('documents.registration')}
+                {t("documents.registration")}
               </div>
             </div>
           </div>
           <div className="bottom_div">
-            <div className="left_">{t('documents.text2')}</div>
+            <div className="left_">{t("documents.text2")}</div>
 
-            <div className="right_">{t('documents.text3')}</div>
+            <div className="right_">{t("documents.text3")}</div>
           </div>
         </div>
         <div className="arc_container">
           <div
             className="top_arc"
-            style={{ top: '-18rem', transform: 'rotate(180deg)' }}
+            style={{ top: "-18rem", transform: "rotate(180deg)" }}
           ></div>
         </div>
         {/* <div className="best_offer second_banner" style={{ top: "12rem" }}>
@@ -115,40 +115,40 @@ const Document = () => {
           <div className="background_ball">
             <div
               className="_low_price"
-              style={{ marginTop: '9rem', fontSize: '1.8rem', color: 'white' }}
+              style={{ marginTop: "9rem", fontSize: "1.8rem", color: "white" }}
             >
-              {t('documents.comprehensive')}{' '}
+              {t("documents.comprehensive")}{" "}
             </div>
             <div
               className="_insurance"
               style={{
-                marginTop: '-11rem',
-                fontSize: '2.5rem',
-                color: 'white',
-                fontWeight: 'bold',
+                marginTop: "-11rem",
+                fontSize: "2.5rem",
+                color: "white",
+                fontWeight: "bold"
               }}
             >
-              {t('documents.formalism')}{' '}
+              {t("documents.formalism")}{" "}
             </div>
           </div>
           <div
             className="trust_us"
-            style={{ position: 'relative', top: '-3rem' }}
+            style={{ position: "relative", top: "-3rem" }}
           >
-            {t('documents.we_provide')}
+            {t("documents.we_provide")}
           </div>
         </div>
         <div className="first_banner">
           <BlurryBlob
             height={90}
             style={{
-              position: 'absolute',
-              top: '50rem',
-              left: 'auto',
-              right: 'auto',
-              marginLeft: '0',
-              marginRight: '0',
-              zIndex: '0 !important',
+              position: "absolute",
+              top: "50rem",
+              left: "auto",
+              right: "auto",
+              marginLeft: "0",
+              marginRight: "0",
+              zIndex: "0 !important"
             }}
           />
         </div>
