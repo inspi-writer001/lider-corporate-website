e88bf846f29f7fb2a06c8111eb8384891fd35986
Emmy <emmyconceptng@gmail.com>
2023-07-12 10:23:39 +0100

    Emmy

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,25 +1,26 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import Backend from "i18next-xhr-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-const fallbackLng = ["pl"];
-const availableLanguages = ["en", "bn"];
+const fallbackLng = ['en'];
+const availableLanguages = ['pl', 'uk', 'en'];
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: 'pl',
     fallbackLng,
     detection: {
-      checkWhitelist: true
+      checkWhitelist: true,
     },
     debug: false,
     whitelist: availableLanguages,
     interpolation: {
-      escapeValue: false // no need for react. it escapes by default
-    }
+      escapeValue: false, // no need for react. it escapes by default
+    },
   });
 
 export default i18n;
